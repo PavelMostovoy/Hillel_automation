ee632b324f1565480e76f1f9ced9ccb685665cf7
Pavel Mostovoy <pavelm@bemobi.com>
2022-08-25 19:44:15 +0200

    example

--- examples/script.py
+++ examples/script.py
@@ -1,6 +1,22 @@
-import data
+import main
 
-print(__name__)
+print(f"My name is '{__name__}'")
 
-if __name__ == "script":
-    print("horrai")
+# if __name__ == "script":
+#     print("horrai")
+
+VARIABE = 23
+
+main.main("ty")
+
+
+def funct(variable):
+    VARIABE = 230
+    internal = variable
+    result = internal + VARIABE
+
+    def new_func():
+        int_2 = VARIABE
+        int_3 = internal
+
+    return result
