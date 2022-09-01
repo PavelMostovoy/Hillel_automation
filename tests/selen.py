3fe1413b2ba131feb49829f3dfb5f65400978ac2
Pavel Mostovoy <pavelm@bemobi.com>
2022-09-01 20:29:36 +0200

    data updated

--- tests/selen.py
+++ tests/selen.py
@@ -1,10 +1,11 @@
 """
-docker run -d --name selenium_chrome -p 4444:4444 -p 5900:5900 -v
-/dev/shm:/dev/shm selenium/standalone-chrome-debug
+docker run -d --name selenium_chrome -p 4444:4444 -p 5900:5900
+selenium/standalone-chrome-debug
 docker rm selenium_chrome
 docker stop selenium_chrome
 docker rm --force selenium_chrome
 """
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,20 +19,33 @@ driver = webdriver.Remote(
         options=options
 )
 
-driver.get("http://google.com")
+driver.get("https://www.aqa.science/admin/")
 
-data_users = {"Alex": "password-1"}
-data_users["Bill"] = "Password2"
+button_identifier = '//*[@id="login-form"]/div[3]/input'
 
-button = driver.find_elements(By.XPATH, '//button')
+field_id = '//*[@id="id_username"]'
 
-# listed below is pseudocode
-field = driver.find_element(By.XPATH, '//field')
-field2 = driver.find_element(By.XPATH, '//field')
+# data_users = {"Alex": "password-1"}
+# data_users["Bill"] = "Password2"
 
-for name, password in data_users.items():
-    password = f"dddd{password}ghrtid"
-    field.send_keys(name)
-    field2.send_keys(password)
 
-    assert button == 1
+field = driver.find_element(By.XPATH, field_id)
+button = driver.find_element(By.XPATH, button_identifier).
+
+field.send_keys("Some name ")
+time.sleep(10)
+
+button.click()
+
+# print(button)
+#
+# # listed below is pseudocode
+# field = driver.find_element(By.XPATH, '//field')
+# field2 = driver.find_element(By.XPATH, '//field')
+#
+# for name, password in data_users.items():
+#     password = f"dddd{password}ghrtid"
+#     field.send_keys(name)
+#     field2.send_keys(password)
+#
+#     assert button == 1
