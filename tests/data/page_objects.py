b8cc95b2a5b999f48c9adec341dc78bedd6e9865
Pavel Mostovoy <pavelm@bemobi.com>
2022-09-05 20:10:43 +0200

    data updated

---
+++ tests/data/page_objects.py
@@ -0,0 +1,4 @@
+submit_button_id = '//*[@id="login-form"]/div[3]/input'
+login_field_id = 'id_username'
+password_field_id = '//*[@id="id_password"]'
+admin_page_header_id = '//*[@id="site-name"]/a'
--- tests/selen.py
+++ tests/selen.py
@@ -6,31 +6,42 @@ docker stop selenium_chrome
 docker rm --force selenium_chrome
 """
 import time
+import json
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from data.page_objects import submit_button_id, login_field_id, \
+    password_field_id, admin_page_header_id
+
+with open(
+        "//data.json",
+        "r") as f:
+    secret_variables = json.load(f)
 
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
-
-# our variables
-submit_button_id = '//*[@id="login-form"]/div[3]/input'
-field_id = '//*[@id="id_username"]'
-field_id_2 = '//*[@id="login-form"]/div[2]'
 
 driver = webdriver.Remote(
         command_executor='http://localhost:4444/wd/hub',
         options=options
 )
 
-driver.get("https://www.aqa.science/admin/")
+driver.get(secret_variables["endpoint"])
 
-field = driver.find_element(By.XPATH, field_id)
-field = driver.find_element(By.XPATH, field_id)
-button = driver.find_element(By.XPATH, submit_button_id)
+name_field = driver.find_element(By.ID, login_field_id)
+submit_button = driver.find_element(By.XPATH, submit_button_id)
+password_field = driver.find_element(By.XPATH, password_field_id)
 
-field.send_keys("Some name ")
-time.sleep(10)
+name_field.send_keys(secret_variables["name"])
+time.sleep(1)
+password_field.send_keys(secret_variables["password"])
 
-button.click()
+submit_button.click()
+time.sleep(1)
+
+element_fo_found = driver.find_element(By.XPATH, admin_page_header_id)
+
+assert element_fo_found.text == "Django administration"
+
+driver.close()
