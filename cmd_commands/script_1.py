5f856b897b6a4dc305f91e602f1fa4ae6bb9234e
Pavel Mostovoy <pavelm@bemobi.com>
2022-09-12 19:06:28 +0200

    data updated

---
+++ cmd_commands/script_1.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+full_path = Path(__file__)
+
+root_path = full_path.parent.parent
+
+file_name = "new_file.txt"
+resources_folder = "resources_1"
+
+file_path = root_path.joinpath(resources_folder, file_name)
+
+print(file_path)
+
+with open(file_path, "w") as f:
+    f.write("    text \nNew line\n")
+
+with open(file_path, "r+") as f:
+    # data = f.readline()
+    # data = f.readline()
+    # f.write(f" updated {data}")
+    new_data = f.read()
+
+print(new_data)
