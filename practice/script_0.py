645440407d7bb53c345567102b8f432b14a3b09f
Pavel Mostovoy <pavelm@bemobi.com>
2022-10-03 19:08:14 +0200

    class

--- practice/script_0.py
+++ practice/script_0.py
@@ -1,15 +1,23 @@
 class OurBilling:
-    order_number = 0
+    def __init__(self, order_number: int, quantity: int, total_price: float):
+        self.order_number = order_number
+        self.product_type = "stuff"
+        self.quantity = quantity
+        self.total_price = total_price
 
 
-# order Nuber
+# order number
 # product type
 # quantity
-# overall price
+# total price
 
 # available or not
 # actual amount
 # return price per item
 
-first = OurBilling()
+
+first = OurBilling(0, 2, 200.0)
 print(first.order_number)
+
+second = OurBilling(1, 3, 150.0)
+print(second.order_number)
