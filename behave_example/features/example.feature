79e7de5a0f3118de76428c23c0b44ce8085121d6
Pavel Mostovoy <pavelm@bemobi.com>
2022-10-31 19:10:27 +0100

    behave example update

--- behave_example/features/example.feature
+++ behave_example/features/example.feature
@@ -1,29 +1,49 @@
 Feature: showing off behave
 
   Background:
-  pass
-
-  Scenario: run a simple test
     Given we have behave installed
     When we implement a test
+
+  Scenario: run a simple test
+
+    Then behave will test it for us!
+
+  @second_test
+  Scenario: Second run a simple test
+
+    Then behave will test it for us!
+    And We win this game
+
+
+  Scenario: Third run a simple test
+
     Then behave will test it for us!
 
 
-  Scenario: run a simple test _ 1
-    Given we have behave installed
-    When we implement a test <User>
-    Then behave will test it for us!
+  Scenario Outline: Name Comparison
+    Given User '<name>'
+    When Add additional user '<name_1>'
+    Then Compare Users names
+    @smoke
+    @regression
+    Examples: first set
+      | name | name_1 | name_3 |
+      | Bob  | Bob    | Bob    |
+      | Mike | Mike   | Mike   |
+      | Bob  | Mike   | Mike   |
+      | Bob  | Bob    | Bob    |
+      | Bill | Mike   | Mike   |
+      | Mike | Mike   | Mike   |
+      | Bob  | Mike   | Mike   |
 
-  Scenario Outline: Blenders
-    Given I put <thing> in a blender,
-    When I switch the blender on
-    Then it should transform into <other thing>
-
-    Examples: Amphibians
-      | thing         | other thing | another  |
-      | Red Tree Frog | mush        | not used |
-
-    Examples: Consumer Electronics
-      | thing        | other thing |
-      | iPhone       | toxic waste |
-      | Galaxy Nexus | toxic waste |
+    @regression
+    @short
+    Examples: Second set
+      | name | name_1 | name_3 |
+      | Bob  | Bob    | Bob    |
+      | Mike | Mike   | Mike   |
+      | Bob  | Mike   | Mike   |
+      | Bob  | Bob    | Bob    |
+      | Bill | Mike   | Mike   |
+      | Mike | Mike   | Mike   |
+      | Bob  | Mike   | Mike   |
--- behave_example/features/steps/example.py
+++ behave_example/features/steps/example.py
@@ -1,8 +1,27 @@
 from behave import *
 
+use_step_matcher("re")
+
+
+class Users:
+    users_list = []
+
+    def get_name(self, item: int):
+        return self.users_list[item]
+
+    def set_name(self, name):
+        self.users_list.append(name)
+
+    def get_list(self):
+        return users.users_list
+
+
+users = Users()
+
 
 @given('we have behave installed')
 def step_impl(context):
+    print("inrernaly added test")
     pass
 
 
@@ -10,42 +29,39 @@ def step_impl(context):
 def step_impl(context):
     assert True is not False
 
-
 @then('behave will test it for us!')
 def step_impl(context):
     assert context.failed is False
 
 
-@when("we implement a test <User>")
+@given("User '(?P<name>.+)'")
+def step_impl(context, name):
+    """
+    :type context: behave.runner.Context
+    :type name: str
+    """
+    context.first_user = name
+
+
+@when("Add additional user '(?P<name>.+)'")
+def step_impl(context, name):
+    """
+    :type context: behave.runner.Context
+    :type name_1: str
+    """
+    context.execute_steps("Given we have behave installed")
+    context.second_user = name
+
+
+@then("Compare Users names")
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    raise NotImplementedError(u'STEP: When we implement a test <User>')
 
-
-@given("I put {thing} in a blender,")
-def step_impl(context, thing):
-    """
-    :type context: behave.runner.Context
-    :type thing: str
-    """
-    raise NotImplementedError(u'STEP: Given I put <thing> in a blender,')
-
-
-@when("I switch the blender on")
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: When I switch the blender on')
-
-
-@then("it should transform into {}")
-def step_impl(context, arg0):
-    """
-    :type context: behave.runner.Context
-    :type arg0: str
-    """
-    raise NotImplementedError(
-            u'STEP: Then it should transform into <other thing>')
+    users.set_name(context.first_user)
+    # first_user = context.first_user
+    second_user = context.second_user
+    assert users.get_name(0) == second_user, \
+        f"Users name are unexpected {users.get_name(0)}, {second_user}, " \
+        f"{users.get_list()} "
